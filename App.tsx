8333c64697c47f1f2e0d29ffd27382490fb182a6
woRixTuR <166818155+woRixTuR@users.noreply.github.com>
2024-04-15 18:02:31 +0200

    Firebase finished

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
 import MainBottomTabNav from "./app/src/navigation/MainBottomTabNav";
+import auth from "@react-native-firebase/auth";
+
+async function authTest(email: string, password: string) {
+  const user = await auth().signInWithEmailAndPassword(email, password);
+  console.log(user);
+}
 
 export default function App() {
   return <MainBottomTabNav />;
