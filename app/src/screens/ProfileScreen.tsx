4688cc9fb1e2e23458cd7dc8cc65851414d3ffc0
woRixTuR <166818155+woRixTuR@users.noreply.github.com>
2024-04-16 17:36:29 +0200

    Update ProfileScreen.tsx

--- app/src/screens/ProfileScreen.tsx
+++ app/src/screens/ProfileScreen.tsx
@@ -1,5 +1,8 @@
+import { Component, ReactNode } from "react";
 import { View } from "react-native";
 
-export function ProfileScreen() {
-  return <View className="flex-1 bg-secondary_color"></View>;
+export class ProfileScreen extends Component {
+  public render(): ReactNode {
+    return <View className="flex-1 bg-secondary_color"></View>;
+  }
 }
