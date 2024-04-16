a840df1b14bfc925a4cddb337c477d99afa4f970
woRixTuR <166818155+woRixTuR@users.noreply.github.com>
2024-04-16 17:35:05 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import MainBottomTabNav from "./app/src/navigation/MainBottomTabNav";
+import { Component, ReactNode } from "react";
+import MainBottomTabNav from "./app/src/navigations/MainBottomTabNav";
 import auth from "@react-native-firebase/auth";
 
-async function authTest(email: string, password: string) {
-  const user = await auth().signInWithEmailAndPassword(email, password);
-  console.log(user);
-}
+export default class App extends Component {
+  private async authTest(email: string, password: string) {
+    const user = await auth().signInWithEmailAndPassword(email, password);
+    console.log(user);
+  }
 
-export default function App() {
-  return <MainBottomTabNav />;
+  public render(): ReactNode {
+    return <MainBottomTabNav />;
+  }
 }
