e9a06affc602f6264d8da459ce2cc1664e3905ba
woRixTuR <worixtur.briar005@outlook.com>
2024-04-22 12:14:47 +0200

    Started using arrow functions

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
-import { Component, ReactNode } from "react";
 import { MainBottomTabNav } from "./app/src/navigations/MainBottomTabNav";
 import auth from "@react-native-firebase/auth";
 
-export default class App extends Component {
-  private async authTest(email: string, password: string) {
-    const user = await auth().signInWithEmailAndPassword(email, password);
-    console.log(user);
-  }
+const authTest = async (email: string, password: string) => {
+  const user = await auth().signInWithEmailAndPassword(email, password);
+  console.log(user);
+};
 
-  public render(): ReactNode {
-    return <MainBottomTabNav />;
-  }
-}
+const App = () => {
+  return <MainBottomTabNav />;
+};
+
+export default App;
