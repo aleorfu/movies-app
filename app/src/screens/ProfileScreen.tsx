877771ef50bfe9d957a8aaa49ba94f31e4c93873
woRixTuR <worixtur.briar005@outlook.com>
2024-04-22 12:42:06 +0200

    ProfileScreen refactorized

--- app/src/screens/ProfileScreen.tsx
+++ app/src/screens/ProfileScreen.tsx
@@ -1,8 +1,7 @@
-import { Component, ReactNode } from "react";
 import { View } from "react-native";
 
-export class ProfileScreen extends Component {
-  public render(): ReactNode {
-    return <View className="flex-1 bg-secondary_color"></View>;
-  }
-}
+const ProfileScreen = () => {
+  return <View className="flex-1 bg-secondary_color"></View>;
+};
+
+export { ProfileScreen };
