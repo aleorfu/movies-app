47266bd0e85c762e143c998a3b78f9e8a5020222
woRixTuR <worixtur.briar005@outlook.com>
2024-04-22 12:46:04 +0200

    HomeScreen refactorized

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -1,8 +1,7 @@
-import { Component, ReactNode } from "react";
 import { View } from "react-native";
 
-export class HomeScreen extends Component {
-  public render(): ReactNode {
-    return <View className="flex-1 bg-secondary_color"></View>;
-  }
-}
+const HomeScreen = () => {
+  return <View className="flex-1 bg-secondary_color"></View>;
+};
+
+export { HomeScreen };
