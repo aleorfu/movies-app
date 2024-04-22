bffebe44e61d66e5a8bb52e8903bf52d6985677a
woRixTuR <worixtur.briar005@outlook.com>
2024-04-22 13:05:56 +0200

    MovieCard refactorized

--- app/src/components/MovieCard.tsx
+++ app/src/components/MovieCard.tsx
@@ -1,61 +1,44 @@
 import { View, Text, Image, ActivityIndicator } from "react-native";
 import { Movie } from "../services/altenHybridApi";
 import { TouchableHighlight } from "react-native-gesture-handler";
-import { Component, ReactNode } from "react";
+import { useState } from "react";
 import { colors } from "../styles/tailwindColors";
 
-export type MovieCardProps = {
-  movie: Movie;
+const MovieCard = ({
+  navigation,
+  movie,
+}: {
   navigation: any;
+  movie: Movie;
+}) => {
+  const [loadingImage, setLoadingImage] = useState(false);
+
+  return (
+    <View className="bg-tertiary_color m-5 flex-col rounded-lg">
+      <TouchableHighlight
+        onPress={() => {
+          const movieId = movie.id;
+          navigation.navigate("Details", { movieId });
+        }}
+      >
+        <View>
+          <Text className="text-quaternary_color text-center font-extrabold text-xl my-2.5">
+            {movie.name}
+          </Text>
+          <Image
+            source={{ uri: movie.pictureUrl }}
+            className="aspect-square rounded-bl-lg rounded-br-lg"
+            resizeMode="cover"
+            onLoadStart={() => setLoadingImage(true)}
+            onLoadEnd={() => setLoadingImage(false)}
+          />
+          {loadingImage && (
+            <ActivityIndicator size="large" color={colors.tertiary_color} />
+          )}
+        </View>
+      </TouchableHighlight>
+    </View>
+  );
 };
 
-export type MovieCardState = {
-  loadingImage: boolean;
-};
-
-export class MovieCard extends Component<MovieCardProps, MovieCardState> {
-  public constructor(props: MovieCardProps) {
-    super(props);
-
-    this.state = {
-      loadingImage: false,
-    };
-  }
-
-  private openDetails(): void {
-    const movieId = this.props.movie.id;
-    this.props.navigation.navigate("Details", { movieId });
-  }
-
-  private startLoading(): void {
-    this.setState({ loadingImage: true });
-  }
-
-  private endLoading(): void {
-    this.setState({ loadingImage: false });
-  }
-
-  public render(): ReactNode {
-    return (
-      <View className="bg-tertiary_color m-5 flex-col rounded-lg">
-        <TouchableHighlight onPress={this.openDetails}>
-          <View>
-            <Text className="text-quaternary_color text-center font-extrabold text-xl my-2.5">
-              {this.props.movie.name}
-            </Text>
-            <Image
-              source={{ uri: this.props.movie.pictureUrl }}
-              className="aspect-square rounded-bl-lg rounded-br-lg"
-              resizeMode="cover"
-              onLoadStart={this.startLoading}
-              onLoadEnd={this.endLoading}
-            />
-            {this.state.loadingImage && (
-              <ActivityIndicator size="large" color={colors.tertiary_color} />
-            )}
-          </View>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}
+export { MovieCard };
