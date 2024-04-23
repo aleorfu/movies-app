ed6dfdd2dd6dbf2c467b89e471517977d31383f7
woRixTuR <worixtur.briar005@outlook.com>
2024-04-23 16:31:29 +0200

    Pretier screens

--- app/src/screens/profile/SignInScreen.tsx
+++ app/src/screens/profile/SignInScreen.tsx
@@ -1,8 +1,11 @@
-import { Text, TouchableHighlight, View } from "react-native";
+import { View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { Button } from "../../components/Button";
 import auth from "@react-native-firebase/auth";
 import { useState } from "react";
+
+const textInputClassName =
+  "text-primary_color bg-quaternary_color mx-5 my-3 p-3 rounded-md";
 
 const signIn = (email: string, password: string) => {
   if (email != "" && password != "")
@@ -17,20 +20,24 @@ const SignInScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View className="flex-1 bg-secondary_color">
+    <View className="flex-1 bg-secondary_color justify-center">
       <TextInput
+        className={textInputClassName}
         keyboardType="email-address"
         onChangeText={(text) => {
           setEmail(text);
         }}
         value={email}
+        placeholder="Email"
       />
       <TextInput
+        className={textInputClassName}
         secureTextEntry={true}
         onChangeText={(text) => {
           setPassword(text);
         }}
         value={password}
+        placeholder="Password"
       />
       <Button
         text="Sign-In"
@@ -39,9 +46,12 @@ const SignInScreen = () => {
           setEmail("");
           setPassword("");
         }}
+        buttonClassName="bg-tertiary_color p-3 mx-5 my-3 rounded-md"
+        textClassName="text-quaternary_color"
       />
       <Button
         text="I forgot the password"
+        buttonClassName="mx-5 my-3"
         textClassName="text-quaternary_color"
         onPress={() => {
           recoverPassword(email);
--- app/src/screens/profile/SignUpScreen.tsx
+++ app/src/screens/profile/SignUpScreen.tsx
@@ -4,6 +4,9 @@ import { Button } from "../../components/Button";
 import { useState } from "react";
 import auth from "@react-native-firebase/auth";
 
+const textInputClassName =
+  "text-primary_color bg-quaternary_color mx-5 my-3 p-3 rounded-md";
+
 const signUp = (email: string, password: string) => {
   if (email != "" && password != "")
     auth().createUserWithEmailAndPassword(email, password);
@@ -13,23 +16,29 @@ const SignUpScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View>
+    <View className="flex-1 bg-secondary_color justify-center">
       <TextInput
+        className={textInputClassName}
         keyboardType="email-address"
         onChangeText={(text) => {
           setEmail(text);
         }}
         value={email}
+        placeholder="Email"
       />
       <TextInput
+        className={textInputClassName}
         secureTextEntry={true}
         onChangeText={(text) => {
           setPassword(text);
         }}
         value={password}
+        placeholder="Password"
       />
       <Button
         text="Sign-Up"
+        buttonClassName="bg-tertiary_color p-3 mx-5 my-3 rounded-md"
+        textClassName="text-quaternary_color"
         onPress={() => {
           signUp(email, password);
           setEmail("");
