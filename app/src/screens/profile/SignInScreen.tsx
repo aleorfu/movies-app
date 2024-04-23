09500088712ad885d8df7f484cff7b3b3d14b9f7
woRixTuR <worixtur.briar005@outlook.com>
2024-04-23 16:13:02 +0200

    Auth finished, now refactor and upgrading

--- app/src/screens/profile/SignInScreen.tsx
+++ app/src/screens/profile/SignInScreen.tsx
@@ -1,15 +1,53 @@
-import { Button, Text, TouchableHighlight, View } from "react-native";
+import { Text, TouchableHighlight, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import { Button } from "../../components/Button";
+import auth from "@react-native-firebase/auth";
+import { useState } from "react";
+
+const signIn = (email: string, password: string) => {
+  if (email != "" && password != "")
+    auth().signInWithEmailAndPassword(email, password);
+};
+
+const recoverPassword = (email: string) => {
+  auth().sendPasswordResetEmail(email);
+};
 
 const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View className="flex-1 bg-secondary_color">
-      <TextInput keyboardType="email-address" />
-      <TextInput secureTextEntry={true} />
-      <Button title="Sign-In" />
-      <TouchableHighlight>
-        <Text className="text-quaternary_color">I forgot the password</Text>
-      </TouchableHighlight>
+      <TextInput
+        keyboardType="email-address"
+        onChangeText={(text) => {
+          setEmail(text);
+        }}
+        value={email}
+      />
+      <TextInput
+        secureTextEntry={true}
+        onChangeText={(text) => {
+          setPassword(text);
+        }}
+        value={password}
+      />
+      <Button
+        text="Sign-In"
+        onPress={() => {
+          signIn(email, password);
+          setEmail("");
+          setPassword("");
+        }}
+      />
+      <Button
+        text="I forgot the password"
+        textClassName="text-quaternary_color"
+        onPress={() => {
+          recoverPassword(email);
+          setEmail("");
+        }}
+      />
     </View>
   );
 };
--- app/src/screens/profile/SignUpScreen.tsx
+++ app/src/screens/profile/SignUpScreen.tsx
@@ -1,7 +1,43 @@
 import { View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Button } from "../../components/Button";
+import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+
+const signUp = (email: string, password: string) => {
+  if (email != "" && password != "")
+    auth().createUserWithEmailAndPassword(email, password);
+};
 
 const SignUpScreen = () => {
-  return <View></View>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View>
+      <TextInput
+        keyboardType="email-address"
+        onChangeText={(text) => {
+          setEmail(text);
+        }}
+        value={email}
+      />
+      <TextInput
+        secureTextEntry={true}
+        onChangeText={(text) => {
+          setPassword(text);
+        }}
+        value={password}
+      />
+      <Button
+        text="Sign-Up"
+        onPress={() => {
+          signUp(email, password);
+          setEmail("");
+          setPassword("");
+        }}
+      />
+    </View>
+  );
 };
 
 export { SignUpScreen };
