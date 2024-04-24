be9c53f90590ace7a38f309481a405992b92750a
woRixTuR <worixtur.briar005@outlook.com>
2024-04-24 10:59:53 +0200

    Added async storage

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -1,7 +1,13 @@
 import { View } from "react-native";
+import { Button } from "../components/Button";
+import messaging from "@react-native-firebase/messaging";
 
 const HomeScreen = () => {
-  return <View className="flex-1 bg-secondary_color"></View>;
+  return (
+    <View className="flex-1 bg-secondary_color">
+      <Button text="Suscribe" />
+    </View>
+  );
 };
 
 export { HomeScreen };
