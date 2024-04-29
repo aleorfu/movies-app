ef074048da7a3dee99654852c0481fb72e4188e0
aleorfu <168004083+aleorfu@users.noreply.github.com>
2024-04-29 12:29:26 +0200

    Added StatusBar and SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import { MainNav } from "./app/src/navigations/MainNav";
 import { PermissionsAndroid } from "react-native";
+import { StatusBar } from "expo-status-bar";
 PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 const App = () => {
-  return <MainNav />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <MainNav />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
 };
 
 export default App;
