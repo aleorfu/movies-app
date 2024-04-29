6b83f4c946f163adef7107de928156294d528188
aleorfu <alejandro.ortiz.fuentes@proton.me>
2024-04-29 09:51:49 +0200

    Added api features

--- app/src/services/altenHybridApi.tsx
+++ app/src/services/altenHybridApi.tsx
@@ -1,5 +1,11 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+type Rating = {
+  userId: string;
+  rating: number;
+  comment: string;
+};
+
 type Movie = {
   id: string;
   name: string;
@@ -9,6 +15,9 @@ type Movie = {
   description: string;
   categories: string[];
   actors: string[];
+  ratings: Rating[];
+  likes: number;
+  userLiked: string[];
 };
 
 const instance = axios.create({
@@ -19,7 +28,7 @@ instance.defaults.headers.common["Accept"] = "application/json";
 const getPetition = async (
   url: string,
   config: AxiosRequestConfig = {}
-): Promise<Movie | Movie[]> => {
+): Promise<Movie | Movie[] | never> => {
   try {
     const response = await instance.get(url, config);
     return response.data;
@@ -29,10 +38,37 @@ const getPetition = async (
   }
 };
 
-const getMovieByIdApi = async (id: string | number): Promise<Movie> =>
+const putPetition = async (
+  url: string,
+  config: AxiosRequestConfig = {}
+): Promise<Movie | never> => {
+  try {
+    const response = await instance.put(url, config);
+    return response.data;
+  } catch (error) {
+    console.error(`There has been an error with api put petition: ${error}`);
+    throw error;
+  }
+};
+
+const getMovieByIdApi = async (id: string): Promise<Movie | never> =>
   (await getPetition(`/movies/${id}`)) as Movie;
 
-const getAllMoviesApi = async (): Promise<Movie[]> =>
+const getAllMoviesApi = async (): Promise<Movie[] | never> =>
   Object.values(await getPetition("/movies"));
 
-export { getMovieByIdApi, getAllMoviesApi, Movie };
+const rateMovie = async (id: string, rating: Rating): Promise<Movie | never> =>
+  (await putPetition(`/movies/${id}/rate`, { data: rating })) as Movie;
+
+const likeMovie = async (id: string, userId: string): Promise<void | never> => {
+  await putPetition(`/movies/${id}/like`, { data: { userId: userId } });
+};
+
+export {
+  getMovieByIdApi,
+  getAllMoviesApi,
+  rateMovie,
+  likeMovie,
+  Movie,
+  Rating,
+};
