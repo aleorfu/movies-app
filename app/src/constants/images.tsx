49835cdb9c4379a8c3389d06caeefeed24cceed5
aleorfu <168004083+aleorfu@users.noreply.github.com>
2024-04-30 11:54:42 +0200

    images.tsx refactored

--- app/src/constants/images.tsx
+++ app/src/constants/images.tsx
@@ -1,6 +1,19 @@
-export const images = {
-  back_icon: require("../../assets/img/back-icon.png") as number,
-  home_icon: require("../../assets/img/home-icon.png") as number,
-  list_icon: require("../../assets/img/list-icon.png") as number,
-  profile_icon: require("../../assets/img/profile-icon.png") as number,
+/*
+I you want to add an image into the dictionary you must add it first to "ImageKeys".
+This is because I want "images" to return numbers, which is the data type for images source.
+*/
+
+type ImageKeys = "back_icon" | "home_icon" | "list_icon" | "profile_icon";
+
+type ImageDictionary = {
+  [k in ImageKeys]: number;
 };
+
+const images: ImageDictionary = {
+  back_icon: require("../../assets/img/back-icon.png"),
+  home_icon: require("../../assets/img/home-icon.png"),
+  list_icon: require("../../assets/img/list-icon.png"),
+  profile_icon: require("../../assets/img/profile-icon.png"),
+};
+
+export { images, ImageDictionary };
