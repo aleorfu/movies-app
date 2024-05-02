b10c5c762eba541bd0f4ef202c95cd7fc9fb225d
aleorfu <168004083+aleorfu@users.noreply.github.com>
2024-05-02 13:04:54 +0200

    Update CommentCard.tsx

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -8,17 +8,17 @@ type CommentCardProps = {
 
 class LocalStyle {
   public static getCommentStyle(): string {
-    const commonStyle = "shadow-lg mx-5 mt-5 p-2 rounded-lg";
-    const lightStyle = "bg-primary_light shadow-black";
-    const darkStyle = "bg-primary_dark shadow-white";
+    const commonStyle: string = "shadow-lg mx-5 mt-5 p-2 rounded-lg";
+    const lightStyle: string = "bg-primary_light shadow-black";
+    const darkStyle: string = "bg-primary_dark shadow-white";
 
     return joinClassNames(commonStyle, [lightStyle, darkStyle]);
   }
 
   public static getTextStyle(): string {
-    const commonStyle = "";
-    const lightStyle = "text-quaternary_light";
-    const darkStyle = "text-quaternary_dark";
+    const commonStyle: string = "";
+    const lightStyle: string = "text-quaternary_light";
+    const darkStyle: string = "text-quaternary_dark";
 
     return joinClassNames(commonStyle, [lightStyle, darkStyle]);
   }
