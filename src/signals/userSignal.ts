f3360984a303765d1cf014ed908bdcf0b2b2550a
aleorfu <alejandro.ortiz.fuentes@proton.me>
2024-05-09 10:45:48 +0200

    Changed user type

--- src/signals/userSignal.ts
+++ src/signals/userSignal.ts
@@ -1,7 +1,8 @@
 import { Signal, signal } from "@preact/signals-react";
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import User = FirebaseAuthTypes.User;
 
-type UserType = FirebaseAuthTypes.User | null;
+type UserType = User | null;
 
 const getUserSignal: Signal<UserType> = signal<UserType>(null);
 
