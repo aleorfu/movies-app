1e845c5cfa91022457bb072cd9e0a87b90eb80ac
aleorfu <alejandro.ortiz.fuentes@proton.me>
2024-05-09 15:26:35 +0200

    Button optimized

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,5 @@
-import { Signal, useSignal } from "@preact/signals-react";
 import { colors } from "@src/styles/tailwindColors";
-import React, { Fragment, useCallback } from "react";
+import React, { Fragment, memo, NamedExoticComponent } from "react";
 import {
   ActivityIndicator,
   Image,
@@ -16,57 +15,51 @@ type ButtonProps = {
   textClassName?: string;
   imageClassName?: string;
   disabled?: boolean;
-  onPress?: () => Promise<void>;
+  loading?: boolean;
+  onPress?: () => void;
 };
 
-const Button = ({
-  text,
-  image,
-  buttonClassName,
-  textClassName,
-  imageClassName,
-  disabled = false,
-  onPress,
-}: ButtonProps): React.JSX.Element => {
-  const loading: Signal<boolean> = useSignal<boolean>(false);
-  const isLight: boolean = useColorScheme() === "light";
+const Button: NamedExoticComponent<ButtonProps> = memo(
+  ({
+    text,
+    image,
+    buttonClassName,
+    textClassName,
+    imageClassName,
+    disabled = false,
+    loading = false,
+    onPress,
+  }: ButtonProps): React.JSX.Element => {
+    const isLight: boolean = useColorScheme() === "light";
 
-  const buttonOnPress = useCallback((): void => {
-    if (!onPress) return;
-
-    loading.value = true;
-    onPress().then(() => {
-      loading.value = false;
-    });
-  }, [onPress]);
-
-  return (
-    <TouchableOpacity
-      disabled={loading.value ? true : disabled}
-      className={buttonClassName}
-      onPress={buttonOnPress}
-    >
-      {loading.value ? (
-        <ActivityIndicator
-          size="small"
-          color={isLight ? colors.quaternary_light : colors.quaternary_dark}
-        />
-      ) : (
-        <Fragment>
-          {image && (
-            <Image
-              source={image}
-              className={imageClassName}
-              tintColor={
-                isLight ? colors.quaternary_light : colors.quaternary_dark
-              }
-            />
-          )}
-          {text && <Text className={textClassName}>{text}</Text>}
-        </Fragment>
-      )}
-    </TouchableOpacity>
-  );
-};
+    return (
+      <TouchableOpacity
+        disabled={loading ? true : disabled}
+        className={buttonClassName}
+        onPress={onPress}
+      >
+        {loading ? (
+          <ActivityIndicator
+            size="small"
+            color={isLight ? colors.quaternary_light : colors.quaternary_dark}
+          />
+        ) : (
+          <Fragment>
+            {image && (
+              <Image
+                source={image}
+                className={imageClassName}
+                tintColor={
+                  isLight ? colors.quaternary_light : colors.quaternary_dark
+                }
+              />
+            )}
+            {text && <Text className={textClassName}>{text}</Text>}
+          </Fragment>
+        )}
+      </TouchableOpacity>
+    );
+  },
+);
 
 export { Button, ButtonProps };
