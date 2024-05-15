85641648a60b4a6c5b310f36fe4257f5c4f87aa0
aleorfu <alejandro.ortiz.fuentes@proton.me>
2024-05-15 15:52:06 +0200

    Now profile picture is outside the nav

--- src/navigations/ProfileTabNav.tsx
+++ src/navigations/ProfileTabNav.tsx
@@ -2,13 +2,24 @@ import {
   createMaterialTopTabNavigator,
   MaterialTopTabNavigationOptions,
 } from "@react-navigation/material-top-tabs";
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { colors } from "@src/styles/tailwindColors";
-import { useColorScheme } from "react-native";
+import { Image, TouchableOpacity, useColorScheme, View } from "react-native";
 import { DocumentsScreen } from "@src/screens/profile/DocumentsScreen";
 import { ProfileStackNav } from "@src/navigations/ProfileStackNav";
+import { Signal, useSignal } from "@preact/signals-react";
+import * as ImagePicker from "expo-image-picker";
+import { ImagePickerResult, MediaTypeOptions } from "expo-image-picker";
+import { getProfilePicture, setProfilePicture } from "@src/services/firebase";
+import { getUserSignal } from "@src/signals/userSignal";
 
 const Tab = createMaterialTopTabNavigator();
+
+const style = {
+  view: "bg-primary_light dark:bg-primary_dark",
+  image:
+    "bg-quaternary_light dark:bg-quaternary_dark w-20 h-20 mx-auto mt-5 rounded-full",
+};
 
 const getOptions = (
   isLight: boolean,
@@ -31,22 +42,69 @@ const getOptions = (
   };
 };
 
+const selectPicture = (
+  userId: string,
+  profilePictureSignal: Signal<string | undefined>,
+): void => {
+  const options: ImagePicker.ImagePickerOptions = {
+    mediaTypes: MediaTypeOptions.Images,
+    selectionLimit: 1,
+  };
+
+  ImagePicker.launchImageLibraryAsync(options).then(
+    (result: ImagePickerResult): void => {
+      if (result.canceled) return;
+      if (result.assets[0].fileName?.split(".").pop() != "jpg") return;
+      profilePictureSignal.value = result.assets[0].uri;
+      setProfilePicture(userId, result.assets[0]);
+    },
+  );
+};
+
 const ProfileTabNav = (): React.JSX.Element => {
+  const profilePictureSignal = useSignal<string | undefined>(undefined);
+
   const isLight = useColorScheme() === "light";
+  const localUser = getUserSignal.value;
+
+  const handleOnImagePress = (): void => {
+    selectPicture(localUser!!.uid, profilePictureSignal);
+  };
+
+  useEffect(() => {
+    if (localUser === null) return;
+    const handleGetProfilePictureSuccess = (url: string): void => {
+      profilePictureSignal.value = url;
+    };
+
+    getProfilePicture(localUser.uid).then(handleGetProfilePictureSuccess);
+  }, []);
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="ProfileInfoTab"
-        component={ProfileStackNav}
-        options={getOptions(isLight, "Info")}
-      />
-      <Tab.Screen
-        name="ProfileDocumentsTab"
-        component={DocumentsScreen}
-        options={getOptions(isLight, "Documents")}
-      />
-    </Tab.Navigator>
+    <Fragment>
+      {localUser?.emailVerified && (
+        <View className={style.view}>
+          <TouchableOpacity onPress={handleOnImagePress}>
+            <Image
+              source={{ uri: profilePictureSignal.value }}
+              className={style.image}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
+      <Tab.Navigator>
+        <Tab.Screen
+          name="ProfileInfoTab"
+          component={ProfileStackNav}
+          options={getOptions(isLight, "Info")}
+        />
+        <Tab.Screen
+          name="ProfileDocumentsTab"
+          component={DocumentsScreen}
+          options={getOptions(isLight, "Documents")}
+        />
+      </Tab.Navigator>
+    </Fragment>
   );
 };
 
